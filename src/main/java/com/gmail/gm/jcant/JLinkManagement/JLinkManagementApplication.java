b12d5f35e57864d9ba52464609dfcc181166504f
jcant <jcant.gm@gmail.com>
2018-07-11 12:48:45 +0300

    add articles to init function

--- src/main/java/com/gmail/gm/jcant/JLinkManagement/JLinkManagementApplication.java
+++ src/main/java/com/gmail/gm/jcant/JLinkManagement/JLinkManagementApplication.java
@@ -60,8 +60,8 @@ public class JLinkManagementApplication extends WebMvcConfigurationSupport{
 
 	@Bean
 	public CommandLineRunner demo(
-		final JUserService userService, 
-		final JLinkService linkService, 
+		final JUserService userService,
+		final JLinkService linkService,
 		final JRootLinkService rlinkService,
 		final JArticleService articleService,
 		final JAdvertisingService advService) {
@@ -74,64 +74,80 @@ public class JLinkManagementApplication extends WebMvcConfigurationSupport{
 //				userService.addUser(admin);
 //				userService.addUser(ouser);
 //
-//				JArticle art1 = new JArticle(new Date(), null, null, "Weclome!", 
+//				JArticle art1 = new JArticle(new Date(), null, null, "Weclome!",
 //					"It's good to see you here!<br>"+
 //					"This is a test project of the redirect platform. You can try the user and admin functionality, as well as the basic <strong>redirect functionality.</strong><br>"+
 //					"In view of hosting restrictions, the redirect works only on references of the type http://sitename.com/your_link</br>"+
 //					"</br>"+
-//					"You can register a new user or log in as an administrator or ordinaly user:", 
+//					"You can register a new user or log in as an administrator or ordinaly user:",
 //					admin);
 //
-//				JArticle art2 = new JArticle(new Date(), null, null, "Admin", 
+//				JArticle art2 = new JArticle(new Date(), null, null, "Admin",
 //					"to Auth as an ADMIN, please input:<br>"+
 //					"<br>"+
 //					"login: <strong>admin</strong><br>"+
-//					"password: <strong>password</strong>", 
+//					"password: <strong>password</strong>",
 //					admin);
 //
-//				JArticle art3 = new JArticle(new Date(), null, null, "User", 
-//					"to Auth as an USER, please input:<br>"+
-//					"<br>"+
-//					"login: <strong>user</strong><br>"+
-//					"password: <strong>password</strong>", 
-//					admin);
-//				
-//				articleService.addArticle(art1);
-//				articleService.addArticle(art2);
-//				articleService.addArticle(art3);
-//				
-//				JAdvertising adv1 = new JAdvertising("jCant Graduate Project", null, null, "Link Management", 
-//						"Project sources: " + 
+				// JArticle art3 = new JArticle(new Date(), null, null, "User", 
+				// 	"to Auth as an USER, please input:<br>"+
+				// 	"<br>"+
+				// 	"login: <strong>user</strong><br>"+
+				// 	"password: <strong>password</strong>",
+				// 	admin);
+        //
+        // JArticle art4 = new JArticle(new Date(), null, null, "Redirecting",
+        //   "Due to "heroku" limitations You may create links based on only one root link: <strong>short-domain-name.herokuapp.com</strong><br>"+
+        //   "and only like <strong>short-domain-name.herokuapp.com/YOURWORLD</strong><br>"+
+        //   "(you can't create links like this: <strong>YOURWORLD.short-domain-name.herokuapp.com</strong>)",
+        //   admin);
+        //
+        // JArticle art5 = new JArticle(new Date(), null, null, "Redirecting",
+        //   "FreeLink (owner admin) <a target=\"_blank\" href=\"https://short-domain-name.herokuapp.com/qqwwee\"><strong>https://short-domain-name.herokuapp.com/qqwwee</strong></a> => google.com?search=qwerty<br>"+
+        //   "FreeLink (owner user) <a target=\"_blank\" href=\"https://short-domain-name.herokuapp.com/aassdd\"><strong>https://short-domain-name.herokuapp.com/aassdd</strong></a> => yahoo.com<br>"+
+        //   "PaidLink (owner admin) <a target=\"_blank\" href=\"https://short-domain-name.herokuapp.com/superadminlink\"><strong>https://short-domain-name.herokuapp.com/superadminlink</strong></a> => gmail.com<br>",
+        //   "PaidLink (owner user) <a target=\"_blank\" href=\"https://short-domain-name.herokuapp.com/superuserlink\"><strong>https://short-domain-name.herokuapp.com/superuserlink</strong></a> => github.com<br>",
+        //   admin);
+        //
+        //
+				// articleService.addArticle(art1);
+				// articleService.addArticle(art2);
+				// articleService.addArticle(art3);
+        // articleService.addArticle(art4);
+        // articleService.addArticle(art5);
+//
+//				JAdvertising adv1 = new JAdvertising("jCant Graduate Project", null, null, "Link Management",
+//						"Project sources: " +
 //						"<a target=\"_blank\" href=\"https://github.com/jcant/JLinkManagement\"> GitHub</a>");
-//				
-//				JAdvertising adv2 = new JAdvertising("prog.kiev.ua", null, null, "Java Courses", 
-//						"excellent Java Academy<br>" + 
+//
+//				JAdvertising adv2 = new JAdvertising("prog.kiev.ua", null, null, "Java Courses",
+//						"excellent Java Academy<br>" +
 //						"<a target=\"_blank\" href=\"https://prog.kiev.ua/\">https://prog.kiev.ua</a>");
-//				
+//
 //				advService.addAdvertising(adv1);
 //				advService.addAdvertising(adv2);
-//				
+//
 //				JRootLink rl1 = new JRootLink("short3-domain-name.herokuapp.com", false);
 //				JRootLink rl2 = new JRootLink("short2-domain-name.herokuapp.com", false);
 //				JRootLink rl3 = new JRootLink("short-domain-name.herokuapp.com", true);
-//				
+//
 //				rlinkService.addRootLink(rl1);
 //				rlinkService.addRootLink(rl2);
 //				rlinkService.addRootLink(rl3);
-//				
+//
 //				Date dstart = JDate.incDay(JDate.setTime(new Date(), "0:0:1"), -1);
 //				Date dfinish = JDate.incMonth(JDate.setTime(new Date(), "0:0:1"), 2);
 //				JLink l1 = new JLink("https://short-domain-name.herokuapp.com/qqwwee", "google.com", admin, dstart, dfinish, true, true);
 //				JLink l2 = new JLink("https://short-domain-name.herokuapp.com/superadminlink", "gmail.com", admin, dstart, dfinish, true, false);
 //				JLink l3 = new JLink("https://short-domain-name.herokuapp.com/aassdd", "yahoo.com", ouser, dstart, dfinish, true, true);
 //				JLink l4 = new JLink("https://short-domain-name.herokuapp.com/superuserlink", "github.com", ouser, dstart, dfinish, true, false);
-//				
+//
 //				linkService.addLink(l1);
 //				linkService.addLink(l2);
 //				linkService.addLink(l3);
 //				linkService.addLink(l4);
-				
-				
+
+
 			}
 		};
 	}
